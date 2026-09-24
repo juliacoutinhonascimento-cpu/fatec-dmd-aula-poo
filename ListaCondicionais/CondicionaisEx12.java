7c277d3d13daad689b40e48fd880cbb1931275c4
juliacoutinhonascimento-cpu <juliacoutinhonascimento@gmail.com>
2026-09-24 20:46:04 +0000

    Exercício 12 de Condicionais

---
+++ ListaCondicionais/CondicionaisEx12.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class CondicionaisEx12 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o número para saber o dia da semana:");
+        int dia = sc.nextInt();
+
+        switch (dia){
+            case 1:
+                System.out.println("Domingo");
+               break;
+
+            case 2:
+                System.out.println("Segunda-feira");
+                break;
+
+            case 3:
+                System.out.println("Terça-feira");
+                break;
+
+            case 4:
+                System.out.println("Quarta-feira");
+                break;
+            
+            case 5:
+                System.out.println("Quinta-feira");
+                break;
+
+            case 6:
+                System.out.println("Sexta-feira");
+                break;
+            
+            case 7:
+                System.out.println("Sábado");
+                break;  
+
+            default:
+                System.out.println("Número inválido");
+        }
+
+        sc.close();
+        }
+}
