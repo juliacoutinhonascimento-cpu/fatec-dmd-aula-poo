33100eb004821c3dcb8969e1b9babadf767bad0a
juliacoutinhonascimento-cpu <juliacoutinhonascimento@gmail.com>
2026-09-24 16:58:31 +0000

    Exercício 4 de Laços de Repetição

---
+++ ListaLacosRepeticao/LacosEx4.java
@@ -0,0 +1,14 @@
+public class LacosEx4 {
+
+    public static void main(String[] args){
+
+        int soma = 0;
+
+        for (int i = 1; i <= 100; i++){
+            
+        }
+
+        System.out.println("Soma: " + soma);
+    }
+
+}
