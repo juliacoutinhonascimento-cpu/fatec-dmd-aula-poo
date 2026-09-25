4d96ecc29835196f5495ee42ed7dd6a85ea44b6a
juliacoutinhonascimento-cpu <juliacoutinhonascimento@gmail.com>
2026-09-25 17:18:58 +0000

    Exercício 10 de Laços de Repetição

--- ListaLacosRepeticao/LacosEx10.java
+++ ListaLacosRepeticao/LacosEx10.java
@@ -4,19 +4,17 @@ public class LacosEx10 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int soma = 0;
+        int numero = 0;
 
-        for (int i = 1; i != 0; i++){
-            System.out.println("Digite o " + i + "° número: ");
-            int numero = sc.nextInt();
-
-            if (numero == 0){
-                break;
-            }
-            
+        do {
+            System.out.println("Digite o número para somar (digite 0 para sair): ");
+            numero = sc.nextInt();
             soma += numero;
-        }
 
-        System.out.println("A soma dos números é: " + soma);
+        } while (numero != 0);{
+            
+            System.out.println("A soma dos números digitados é: " + soma);
+        }
         sc.close();
     }
 
